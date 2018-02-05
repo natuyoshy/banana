3561b472db947071282e675014702ac08d254c21
user <user@usernoMacBook-Pro.local>
2018-02-05 12:59:12 +0900

    json_tweak

--- test/hello_world.py
+++ test/hello_world.py
@@ -1,18 +1,21 @@
 from bottle import route , run,request
 import json
 
+dict = {"meesage":"hello"}
+
 @route('/<item_id>')
 def modify_item(item_id):
-
     return  item_id+'さんこんにちは！'
 @route('/morning/<user>')
-def morning_user(user):
-    return user+'さん、おはよ！'
+def api(user):
+    a = user+',good morning!'
+    dict["meesage"] = a
+    return dict
 @route('/afternoon/<user>')
 def api(user):
-    # return data+'さん、こんにちは！'
-    a =json.dumps([user+'hello!'])
-    return a
+    a =json.dumps([user+',hello!'])
+    dict["meesage"] = a
+    return dict
 
 
 run(hots='localhost',port=8088)
