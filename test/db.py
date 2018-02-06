bd7477099e8b0a903584f5d5a4dafb8a8b74c5c7
Natsumi Kobayashi <z1167474@hotmail.co.jp>
2018-02-06 11:51:16 +0900

    dbでいろいろ

---
+++ test/db.py
@@ -0,0 +1,16 @@
+import dataset
+
+db = dataset.connect('mysql://roots:rootroot@127.0.0.1:3311/mysql')
+
+table = db['sample']
+#insert
+table.insert(dict(id=4,name='arakita'))
+#update
+data = dict(id=4, name='arakita')
+table.upsert(data, ['id'])
+#delete
+table.delete(name='arakita')
+#select
+results = table.find(order_by='id')
+for r in results:
+    print(r)
