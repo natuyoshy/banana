e48dc3f11b2201642c62e122d0560aedd2183a35
Natsumi Kobayashi <z1167474@hotmail.co.jp>
2018-02-06 18:43:40 +0900

    jsonでのデータ挿入変更とか

--- test/web_db.py
+++ test/web_db.py
@@ -4,48 +4,38 @@ import json
 db = dataset.connect('mysql://roots:rootroot@127.0.0.1:3311/mysql')
 table = db['sample']
 
-
 @route('/')
 def normal_page():
     return 'wa-i'
 
 @route('/<user_id>',method="get")
 def modify_item(user_id):
-    result = table.find_one(id=user_id)
+    result = json.dumps(table.find_one(id=user_id))
+    return result
 
-    # print (result['id'])
-
-    return ' <form method="post">ID:<input type="text" name="id" value="'+str(result['id'])+'">名前：<input type="text" name="name" value="'+result['name']+'"><br><input type="submit"></form>'
-    # return result['name']+',get'
-
-@route('/<user_id>/<user_name>',method="post")
-def post_page(user_id,user_name):
-    table.insert(dict(id=user_id, name=user_name))
-    return user_id+',post'
+@route('/<user_id>',method="post")
+def post_page(user_id):
+    json = request.json
+    id = str(json['id'])
+    name = json['name']
+    table.insert(dict(id=id, name=name))
+    return id + "の" + name + 'さんを追加しました'
 
 @route('/<user_id>',method="delete")
 def delete_page(user_id):
+    result = table.find_one(id=user_id)
+    id = str(result['id'])
+    name = result['name']
     table.delete(id=user_id)
-    return user_id+',delete'
+    return id+"の"+name+'さんを削除しました'
 
-@route('/<user_id>/<user_name>',method="put")
-def put_page(user_id,user_name):
-    data = dict(id=user_id, name=user_name)
+@route('/<user_id>',method="put")
+def put_page(user_id):
+    json = request.json
+    id = str(json['id'])
+    name = json['name']
+    data = dict(id=id,name=name)
     table.upsert(data, ['id'])
-    return user_id+',put'
-
-
-
-# #insert
-# table.insert(dict(id=4,name='arakita'))
-# #update
-# data = dict(id=4, name='arakita')
-# table.upsert(data, ['id'])
-# #delete
-# table.delete(name='arakita')
-# #select
-# results = table.find(order_by='id')
-# for r in results:
-#     print(r)
+    return id+"の"+name+'の名前を変更しました'
 
 run(host='localhost', port=3309)
