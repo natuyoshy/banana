fd18ca2b236f80b7266f65be4b632d2595d6f189
Natsumi Kobayashi <z1167474@hotmail.co.jp>
2018-02-09 17:30:45 +0900

    add_test

--- gurunavi_api/tests/test_web_db.py
+++ gurunavi_api/tests/test_web_db.py
@@ -1,9 +1,10 @@
 import unittest
+
 from web_db import search_rest
 
 
 class TestStringMethods(unittest.TestCase):
-    def test_search_rest(self):
+    def 余分なテスト(self):
         rests = search_rest({
             "rest":
                 [{"name": 'name',
@@ -11,9 +12,47 @@ class TestStringMethods(unittest.TestCase):
                   "longitude": '563',
                   "category": 'ggg',
                   "url": 'ggg',
+                  "address": 'ggg',
+                  "id": 'dd'}
+                 ]})
+
+        self.assertListEqual(rests, ['name', '567', '563', 'ggg', 'ggg', 'ggg'])
+
+    def 不足なテスト(self):
+        rests = search_rest({
+            "rest":
+                [{"name": 'name',
+                  "latitude": '567',
+                  "longitude": '563',
+                  "category": 'ggg',
                   "address": 'ggg'}
                  ]})
-        self.assertListEqual(rests, ['name', '567', '563', 'ggg','ggg', 'ggg'])
 
-        if __name__ == '__main__':
-            unittest.main()
+        self.assertListEqual(rests, ['name', '567', '563', 'ggg', 'ggg'])
+
+    def ぬるテスト(self):
+        rests = search_rest({
+            "rest":
+                [{}
+                 ]})
+
+        self.assertListEqual(rests, [])
+
+    def 配列テスト(self):
+        rests = search_rest({
+            "rest":
+                [{"name": 'name',
+                  "latitude": '567',
+                  "longitude": '563',
+                  "category": 'ggg',
+                  "address": 'ggg'},
+                 {"name": 'names',
+                  "latitude": '564',
+                  "longitude": '533',
+                  "category": 'gd',
+                  "address": 'gg'}
+                 ]
+        })
+
+        self.assertListEqual(rests, [{'name', '567', '563', 'ggg', 'ggg'}, {'names', '564', '533', 'gd', 'gg'}])
+        self.assertListEqual(len(rests), 2)
